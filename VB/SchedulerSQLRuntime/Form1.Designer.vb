a9028e3dbb0d88b14c0fdfa458e6a4ebf36c85a2
DevExpressExampleBot <fakeEmail>
2020-07-29 15:24:42 +0300

    autogenerate VB

--- VB/SchedulerSQLRuntime/Form1.Designer.vb
+++ VB/SchedulerSQLRuntime/Form1.Designer.vb
@@ -1,74 +1,76 @@
 ﻿Namespace SchedulerSQLRuntime
-    Partial Public Class Form1
-        ''' <summary>
-        ''' Required designer variable.
-        ''' </summary>
-        Private components As System.ComponentModel.IContainer = Nothing
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
 
-        ''' <summary>
-        ''' Clean up any resources being used.
-        ''' </summary>
-        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
-            End If
-            MyBase.Dispose(disposing)
-        End Sub
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
 
-        #Region "Windows Form Designer generated code"
+		#Region "Windows Form Designer generated code"
 
-        ''' <summary>
-        ''' Required method for Designer support - do not modify
-        ''' the contents of this method with the code editor.
-        ''' </summary>
-        Private Sub InitializeComponent()
-            Me.components = New System.ComponentModel.Container()
-            Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
-            Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
-            Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
-            Me.schedulerDataStorage1 = New DevExpress.XtraScheduler.SchedulerDataStorage(Me.components)
-            CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.schedulerDataStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
-            Me.SuspendLayout()
-            ' 
-            ' schedulerControl1
-            ' 
-            Me.schedulerControl1.AllowDrop = False
-            Me.schedulerControl1.DataStorage = Me.schedulerDataStorage1
-            Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
-            Me.schedulerControl1.Location = New System.Drawing.Point(0, 0)
-            Me.schedulerControl1.Name = "schedulerControl1"
-            Me.schedulerControl1.Size = New System.Drawing.Size(784, 562)
-            Me.schedulerControl1.Start = New Date(1753, 1, 1, 0, 0, 0, 0)
-            Me.schedulerControl1.TabIndex = 0
-            Me.schedulerControl1.Text = "schedulerControl1"
-            Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
-            Me.schedulerControl1.Views.GanttView.Enabled = False
-            Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
-            ' 
-            ' schedulerDataStorage1
-            ' 
-            Me.schedulerDataStorage1.AppointmentDependencies.AutoReload = False
-            ' 
-            ' Form1
-            ' 
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-            Me.ClientSize = New System.Drawing.Size(784, 562)
-            Me.Controls.Add(Me.schedulerControl1)
-            Me.Name = "Form1"
-            Me.Text = "Form1"
-            CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.schedulerDataStorage1, System.ComponentModel.ISupportInitialize).EndInit()
-            Me.ResumeLayout(False)
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
+			Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
+			Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+			Me.schedulerDataStorage1 = New DevExpress.XtraScheduler.SchedulerDataStorage(Me.components)
+			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.schedulerDataStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' schedulerControl1
+			' 
+			Me.schedulerControl1.AllowDrop = False
+			Me.schedulerControl1.DataStorage = Me.schedulerDataStorage1
+			Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.schedulerControl1.Location = New System.Drawing.Point(0, 0)
+			Me.schedulerControl1.Name = "schedulerControl1"
+			Me.schedulerControl1.Size = New System.Drawing.Size(784, 562)
+			Me.schedulerControl1.Start = New DateTime(1753, 1, 1, 0, 0, 0, 0)
+			Me.schedulerControl1.TabIndex = 0
+			Me.schedulerControl1.Text = "schedulerControl1"
+			Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
+			Me.schedulerControl1.Views.GanttView.Enabled = False
+			Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+			' 
+			' schedulerDataStorage1
+			' 
+			Me.schedulerDataStorage1.AppointmentDependencies.AutoReload = False
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(784, 562)
+			Me.Controls.Add(Me.schedulerControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
+'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
+			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.schedulerDataStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
 
-        End Sub
+		End Sub
 
-        #End Region
+		#End Region
 
-        Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
-        Private schedulerDataStorage1 As DevExpress.XtraScheduler.SchedulerDataStorage
-    End Class
+		Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+		Private schedulerDataStorage1 As DevExpress.XtraScheduler.SchedulerDataStorage
+	End Class
 End Namespace
 
--- VB/SchedulerSQLRuntime/My Project/Settings.Designer.vb
+++ VB/SchedulerSQLRuntime/My Project/Settings.Designer.vb
@@ -11,23 +11,27 @@
 Namespace My
 
 
-    <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(), Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")> _
-    Friend NotInheritable Partial Class Settings
-        Inherits System.Configuration.ApplicationSettingsBase
+	<Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>
+	<Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")>
+	Friend NotInheritable Partial Class Settings
+		Inherits System.Configuration.ApplicationSettingsBase
 
-        Private Shared defaultInstance As Settings = (CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New Settings()), Settings))
+		Private Shared defaultInstance As Settings = (CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New Settings()), Settings))
 
-        Public Shared ReadOnly Property [Default]() As Settings
-            Get
-                Return defaultInstance
-            End Get
-        End Property
+		Public Shared ReadOnly Property [Default]() As Settings
+			Get
+				Return defaultInstance
+			End Get
+		End Property
 
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(), Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString), Global.System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Data\DXDBScheduler.m" & "df;Integrated Security=True;Connect Timeout=30")> _
-        Public ReadOnly Property DXDBSchedulerConnectionString() As String
-            Get
-                Return (DirectCast(Me("DXDBSchedulerConnectionString"), String))
-            End Get
-        End Property
-    End Class
+		<Global.System.Configuration.ApplicationScopedSettingAttribute()>
+		<Global.System.Diagnostics.DebuggerNonUserCodeAttribute()>
+		<Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString)>
+		<Global.System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Data\DXDBScheduler.m" & "df;Integrated Security=True;Connect Timeout=30")>
+		Public ReadOnly Property DXDBSchedulerConnectionString() As String
+			Get
+				Return (DirectCast(Me("DXDBSchedulerConnectionString"), String))
+			End Get
+		End Property
+	End Class
 End Namespace
